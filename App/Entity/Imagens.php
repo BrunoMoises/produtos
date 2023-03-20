41c61b7465fff21c77302e3c5a9836d797d9cdaa
Bruno <bruno_moises_stz@hotmail.com>
2023-03-20 16:54:34 -0300

    feat: create 'imagens' entity

---
+++ App/Entity/Imagens.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+class Produto
+{
+    private $id;
+    private $produto_id;
+    private $imagem;
+
+    public function __construct($id = '', $produto_id = '', $imagem = '')
+    {
+        $this->id = $id;
+        $this->produto_id = $produto_id;
+        $this->imagem = $imagem;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getProduto_id()
+    {
+        return $this->produto_id;
+    }
+
+    public function setProduto_id($produto_id)
+    {
+        $this->produto_id = $produto_id;
+
+        return $this;
+    }
+
+    public function getImagem()
+    {
+        return $this->imagem;
+    }
+
+    public function setImagem($imagem)
+    {
+        $this->imagem = $imagem;
+
+        return $this;
+    }
+}
