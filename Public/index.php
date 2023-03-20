ab9743a37312c60695ab0ecc097286a56ff3b51b
Bruno <bruno_moises_stz@hotmail.com>
2023-03-20 13:54:22 -0300

    build: create initial structure for project

---
+++ Public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Pedidos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+</head>
+
+<body>
+    <h1>Pagina inicial</h1>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
