58696417eea9dbf03d1e9fbc8250cb3863782258
Bruno <bruno_moises_stz@hotmail.com>
2023-03-20 21:40:16 -0300

    feat: create controller of order

---
+++ App/Controller/PedidoController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pedido;
+use App\Model\PedidoModel;
+
+class PedidoController
+{
+    private $pedidoModel;
+    public function __construct()
+    {
+        $this->pedidoModel = new PedidoModel();
+    }
+    function create($data = null)
+    {
+        $pedido = $this->convertType($data);
+        $result = $this->validate($pedido);
+
+        if ($result != "") {
+            return json_encode(["result" => $result]);
+        }
+
+        return json_encode(["id" => $this->pedidoModel->create($pedido)]);
+    }
+
+    function delete($id = 0)
+    {
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        if ($id <= 0)
+            return json_encode(["result" => "invalid id"]);
+
+        $result = $this->pedidoModel->delete($id);
+
+        return json_encode(["result" => $result]);
+    }
+
+    function readById($id = 0)
+    {
+        $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        if ($id <= 0)
+            return json_encode(["result" => "invalid id"]);
+
+        return $this->pedidoModel->readById($id);
+    }
+
+    function readAll()
+    {
+        return json_encode($this->pedidoModel->readAll());
+    }
+
+    private function convertType($data)
+    {
+        return new Pedido(
+            null,
+            (isset($data['valor']) ? filter_var($data['valor'], FILTER_SANITIZE_NUMBER_FLOAT) : null)
+        );
+    }
+
+    private function validate(Pedido $pedido, $update = false)
+    {
+        if ($update && $pedido->getId() <= 0)
+            return "invalid id";
+
+        if ($pedido->getValor() <= 0)
+            return "invalid valor";
+
+        return "";
+    }
+}
