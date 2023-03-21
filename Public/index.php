59455170516ed074fc0e4585a951cac67f400ec3
Bruno <bruno_moises_stz@hotmail.com>
2023-03-21 15:31:36 -0300

    feat: create page for orders

--- Public/index.php
+++ Public/index.php
@@ -7,12 +7,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 
 <body>
-    <h1>Pagina inicial</h1>
-    <script src="js/script.js"></script>
+    <div class="container">
+        <header>
+            <div class="row mt-3">
+                <div class="col-md-3">
+                    <a href="index.php">CRUD Pedidos</a>
+                </div>
+                <div class="col-md-9 text-start">
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pedidos.php">Pedidos</a>
+                        </li>
+                    </ul>
+                </div>
+        </header>
+        <hr class="border-info">
+        <div>
+            <button type="button" class="btn btn-outline-info" id="bt-new">Novo produto</button>
+        </div>
+        <div class="row mt-3">
+            <table class="table table-striped-columns table-bordered">
+                <thead>
+                    <tr>
+                        <th colspan="3">Codigo</th>
+                        <th>Descrição</th>
+                        <th>Valor</th>
+                        <th>Estoque</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+            <script id="tmplLinha" type="text/template">
+                <tr>
+                    <td width="10"><i class="bi-x-circle-fill icon icon-x pointer" title="Excluir produto"
+                            aria-multiline="Excluir produto" onclick="deleteProduto('{{id_produto}}')"></i></td>
+                    <td width="10"><i class="bi-pencil-fill icon icon-edit pointer" title="Editar produto"
+                            aria-multiline="Editar produto" onclick="editaProduto('{{id_produto}}')"></i></td>
+                    <td align="center" width="40">{{id_produto}}</td>
+                    <td>{{Descricao}}</td>
+                    <td>{{Valor}}</td>
+                    <td>{{Estoque}}</td>
+                </tr>
+            </script>
+        </div>
+    </div>
+    <script src="js/handlebars.js"></script>
+    <script src="js/jquery-3.6.3.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script-produtos.js"></script>
 </body>
 
 </html>
