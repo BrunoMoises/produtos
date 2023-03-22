40a36790b3278869619f63ee3c650cf883fb61d9
Bruno <bruno_moises_stz@hotmail.com>
2023-03-22 13:37:33 -0300

    feat: create initial script for orders with search and populating table of orders

--- Public/js/script-pedidos.js
+++ Public/js/script-pedidos.js
@@ -4,4 +4,41 @@ $(document).ready(function () {
     readAll();
 });
 
-//EVENTOS
+function createTable(data) {
+    if (data.length < 1)
+        return;
+
+    var tabela = document.querySelector(".table tbody");
+    tabela.innerHTML = "";
+
+    var tmplSource = document.getElementById("tmplLinha").innerHTML;
+    var tmplHandle = Handlebars.compile(tmplSource);
+
+    for (var i = 0; i < data.length; i++) {
+        var pedido = {};
+        pedido.id_pedido = data[i].id;
+        pedido.Valor = data[i].valor;
+
+        var linha = {};
+        linha.template = document.createElement("template");;
+        linha.template.innerHTML = tmplHandle(pedido)
+        linha.content = document.importNode(linha.template.content, true);
+
+        tabela.appendChild(linha.content);
+    }
+}
+
+function readAll() {
+    $.ajax({
+        url: "api/pedido",
+        type: "GET",
+        data: {},
+        dataType: "json",
+        success: function (data) {
+            createTable(data);
+        },
+        error: function () {
+            alert("Houve um erro na busca");
+        }
+    });
+}
